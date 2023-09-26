8ebb7509a925a92bdbbd1002683191877bedc758
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-26 13:58:54 -0300

    feat: add movie routes

--- apps/movie-api/src/main.ts
+++ apps/movie-api/src/main.ts
@@ -2,14 +2,15 @@ import express from 'express';
 import * as path from 'path';
 
 const app = express();
-
+const port = process.env.PORT || 3333;
+// app.use(cors({ credentials: true }));
+// app.use(bodyParser.json());
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Welcome to movie-api!' });
 });
 
-const port = process.env.PORT || 3333;
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });

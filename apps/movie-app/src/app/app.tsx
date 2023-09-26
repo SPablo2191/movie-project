50dfc55fccdabc27cd5c9b4c0c490a615828e868
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-26 11:21:15 -0300

    refactor: create pages per module of app

--- apps/movie-app/src/app/app.tsx
+++ apps/movie-app/src/app/app.tsx
@@ -1,14 +1,7 @@
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.scss';
 
-import NxWelcome from './nx-welcome';
-
 export function App() {
-  return (
-    <div className='flex justify-content-start'>
-      <NxWelcome title="movie-app" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;

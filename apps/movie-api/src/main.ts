4f9b602561838b34f38a564920b4d3589f08b4f7
Pablo Sandoval <pablosandoval2191@gmail.com>
2023-09-27 16:36:56 -0300

    feat: define movie router

--- apps/movie-api/src/main.ts
+++ apps/movie-api/src/main.ts
@@ -1,15 +1,15 @@
+import { json } from 'body-parser';
+import { cors } from 'cors';
 import express from 'express';
-import * as path from 'path';
-
+import 'dotenv/config';
+import movieRouter from './routes/movie-routes';
 const app = express();
 const port = process.env.PORT || 3333;
-// app.use(cors({ credentials: true }));
-// app.use(bodyParser.json());
-app.use('/assets', express.static(path.join(__dirname, 'assets')));
+const apiBaseUrl : string = `/api/${process.env.VERSION || 'v1'}`;
+app.use(cors({ credentials: true }));
+app.use(json());
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to movie-api!' });
-});
+app.get(apiBaseUrl, movieRouter);
 
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);

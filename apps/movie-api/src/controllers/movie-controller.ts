e8e51aaf44ad91af248144e5f69886ae154d594a
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-28 12:46:11 -0300

    feat: add status enum

--- apps/movie-api/src/controllers/movie-controller.ts
+++ apps/movie-api/src/controllers/movie-controller.ts
@@ -1,6 +1,7 @@
 import { Request, Response } from 'express';
 import dotenv from 'dotenv';
 import axios from 'axios';
+import { Status } from '../project/enums/status.enum';
 dotenv.config();
 
 const getAllMovies = async (req: Request, res: Response) => {
@@ -9,14 +10,14 @@ const getAllMovies = async (req: Request, res: Response) => {
       `https://${process.env.API_URL}/3/movie/now_playing?api_key=${process.env.API_KEY}&language=${process.env.LANGUAGE}`
     );
 
-    if (response.status !== 200) {
+    if (response.status !== Status.SUCCESS) {
       throw new Error(`Request failed with status: ${response.status}`);
     }
 
-    return res.status(200).json(response.data);
+    return res.status(Status.SUCCESS).json(response.data);
   } catch (error) {
     console.error(error);
-    return res.status(500).json({ error: 'Something went wrong' });
+    return res.status(Status.INTERNAL_SERVER_ERROR).json({ error: 'INTERNAL_SERVER_ERROR' });
   }
 };
 

9ea73c8e229dc861798c769700bc1f771e65ccc3
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-28 11:25:03 -0300

    feat: add enums endpoints and operations

--- apps/movie-api/src/routes/movie-routes.ts
+++ apps/movie-api/src/routes/movie-routes.ts
@@ -1,6 +1,8 @@
 import { Router } from 'express';
 import { getAllMovies } from '../controllers/movie-controller';
+import { Endpoints } from '../project/enums/endpoints.enum';
+import { operations } from '../project/enums/operation.enum';
 const movieRouter = Router();
 
-movieRouter.get('/', getAllMovies);
+movieRouter.get(`${Endpoints.movies}${operations.getAll}`, getAllMovies);
 export default movieRouter;

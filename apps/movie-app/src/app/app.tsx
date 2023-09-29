c908d2f530b2a6ec1553535bacf514aa9072f590
SPablo2191 <pablosandoval2191@gmail.com>
2023-09-29 12:43:33 -0300

    feat: add router in app

--- apps/movie-app/src/app/app.tsx
+++ apps/movie-app/src/app/app.tsx
@@ -1,7 +1,7 @@
-import styles from './app.module.scss';
-
+import { RouterProvider } from 'react-router-dom';
+import { router } from './routes';
 export function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
